f78bb7086a904df6fd160e5869727f8240b1f483
raphaelbmesquita123 <raphaelbmesquita@gmail.com>
2021-04-26 22:22:57 +0100

    card style update

--- src/pages/stores/Stores.js
+++ src/pages/stores/Stores.js
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react'
 import styles from './slug.style.module.scss';
 
-
-
 export function Stores (){
     const [pokemons, setPokemons] = useState([])
+    
 
-    useEffect(() => {
-        for(let i = 1; i <= 5; i++){
-             fethPokemon(i)
-        }
-
-        async function fethPokemon (num) {
-            const url =  `https://pokeapi.co/api/v2/pokemon/${num}`
+    useEffect(async () => {
+        const url =  `https://pokeapi.co/api/v2/pokemon/5`
             const res = await fetch(url)
             const data = await res.json()
-            setPokemons(...pokemons, pokemons.push(data))
-        }
+            const price = Math.floor(Math.random() * 100 + Number(data.base_experience))
+            data.price = price
+            console.log(data)
+            setPokemons(data)
+
+        // for(let i = 1; i <= 5; i++){
+        //     fethPokemon(i)
+        // }
+        
+        // async function fethPokemon (num) {
+        //     const url =  `https://pokeapi.co/api/v2/pokemon/${num}`
+        //     const res = await fetch(url)
+        //     const data = await res.json()
+        //     createPokemonCard(data)
+        //     }
+        // function createPokemonCard(pokemon){
+            
+        // }
+
     }, [])
 
 
@@ -24,11 +35,60 @@ export function Stores (){
     return(
 
         <div className={styles.storeContainer}>
-            <div>
-                {
-                    pokemons.map(pokemon => console.log(pokemon))
-                }
-            </div>
+            
+            {
+                <div className={styles.pokemonCard}>
+                    <div className={styles.pokemonExperience}>
+                        <small>{pokemons.base_experience}</small>
+                        <strong>xp</strong>
+                    </div>
+                    <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemons.id}.png`}/>
+                    <div className={styles.pokemonInfo}> 
+                        <h3>{pokemons.name}</h3>
+                        <small>R$ {pokemons.price}</small>
+                    </div>
+                </div>
+            }
+            {
+                <div className={styles.pokemonCard}>
+                    <div className={styles.pokemonExperience}>
+                        <small>{pokemons.base_experience}</small>
+                        <strong>xp</strong>
+                    </div>
+                    <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemons.id}.png`}/>
+                    <div className={styles.pokemonInfo}> 
+                        <h3>{pokemons.name}</h3>
+                        <small>R$ {pokemons.price}</small>
+                    </div>
+                </div>
+            }
+            {
+                <div className={styles.pokemonCard}>
+                    <div className={styles.pokemonExperience}>
+                        <small>{pokemons.base_experience}</small>
+                        <strong>xp</strong>
+                    </div>
+                    <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemons.id}.png`}/>
+                    <div className={styles.pokemonInfo}> 
+                        <h3>{pokemons.name}</h3>
+                        <small>R$ {pokemons.price}</small>
+                    </div>
+                </div>
+            }
+            {
+                <div className={styles.pokemonCard}>
+                    <div className={styles.pokemonExperience}>
+                        <small>{pokemons.base_experience}</small>
+                        <strong>xp</strong>
+                    </div>
+                    <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemons.id}.png`}/>
+                    <div className={styles.pokemonInfo}> 
+                        <h3>{pokemons.name}</h3>
+                        <small>R$ {pokemons.price}</small>
+                    </div>
+                </div>
+            }
+        
         </div>
     )
 }
